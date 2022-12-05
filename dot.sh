e70c4a7fdc8ba300f2e3a6cadaaa9d9dd8594396
Jan Fooken <git@bode.fun>
2022-12-05 19:31:52 +0100

    Add variables

--- dot.sh
+++ dot.sh
@@ -1,27 +1,27 @@
 #! /usr/bin/sh
 
+DOT_WORKING_TREE="$HOME"
+DOT_GIT_DIR="$HOME"/.dotfiles
+DOT_BACKUP_DIR="$HOME"/.dotfiles-backup
+
 dot() {
-    $(which git) --git-dir="$HOME"/.dotfiles/ --work-tree="$HOME" "$@"
+    $(which git) --git-dir="$DOT_GIT_DIR"/ --work-tree="$DOT_WORKING_TREE" "$@"
 }
 
-dot_install() {
-    git clone --bare "$1" "$HOME"/.dotfiles > /dev/null
-
-    if dot checkout > /dev/null; then
-        echo "Dotfiles cloned";
+dot_install() { 
+    git clone --bare "$1" "$DOT_GIT_DIR"
+    if dot checkout; then
+        echo "Dotfiles cloned"
     else
-        echo "Backing up pre-existing dot files.";
-        mkdir -p "$HOME"/.dotfiles-backup
-        dot checkout 2>&1 | grep -E "\s+\." | awk '{print $1}' | xargs -I{} mv {} "$HOME"/.dotfiles-backup/{}
-
-
-        if dot checkout > /dev/null; then
-            echo "Dotfiles cloned";
+        echo "Backing up pre-existing dot files."
+        mkdir -p "$DOT_BACKUP_DIR"
+        dot checkout 2>&1 | grep -E "\s+\." | awk '{print $1}' | xargs -I{} mv {} "$DOT_BACKUP_DIR"/{}
+        if dot checkout; then
+            echo "Dotfiles cloned"
         else
-            echo "Can not clone dotfiles";
-	        exit 1
-        fi;
-    fi;
-
+            echo "Can not clone dotfiles"
+	    exit 1
+        fi
+    fi
     dot config status.showUntrackedFiles no
 }
